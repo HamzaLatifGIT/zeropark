eb11cc2c4e43e5f494f287856ce2696598ad4e38
JunaidLatif4 <junaidlatif004@gmail.com>
2024-08-18 15:44:50 +0500

    brand and agencies responsive bug fix

--- src/components/pages/Brand and Agencies/BrandsConnect.jsx
+++ src/components/pages/Brand and Agencies/BrandsConnect.jsx
@@ -14,7 +14,7 @@ function BrandsConnect() {
             <div className="brandconnect__container max-width">
                 <div className='heading' data-aos='fade-up' data-aos-delay='100'> <h2>We help brands connect with consumers <br />when they're most likely to convert.</h2> <p>You help customers discover brands and products they love when and where it matters the most.</p></div>
                 <div className='brandconnect__box'>
-                    <div className='text' data-aos='fade-up' data-aos-delay='200'><p>The current state of online commerce requires brands to answer every customer interaction, no matter how fleeting.</p><p data-aos='fade-up' data-aos-delay='300'>That’s why creating shoppable moments that help streamline the consumer journey from inspiration to purchase is what seamless customer experience is all about.</p></div>
+                    <div className='text' data-aos='fade-up' data-aos-delay='200'><p style={{fontWeight:'700'}}>The current state of online commerce requires brands to answer every customer interaction, no matter how fleeting.</p><p data-aos='fade-up' data-aos-delay='300'>That’s why creating shoppable moments that help streamline the consumer journey from inspiration to purchase is what seamless customer experience is all about.</p></div>
                     <div className="icons">
                         <div className="icon" data-aos='fade-up' data-aos-delay='300'><img src={icon1} alt="" data-aos='fade-in' data-aos-delay='600' /><span>Capturing customers at key touchpoints</span></div>
                         <div className="icon" data-aos='fade-up' data-aos-delay='400'><img src={icon2} alt="" data-aos='fade-in' data-aos-delay='700' /><span>Seamless experience</span></div>
